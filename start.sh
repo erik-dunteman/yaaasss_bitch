26246a4ae6a5584d27fac62c6192744f915ab781
Erik Dunteman <erikdunteman@gmail.com>
2019-09-03 19:30:47 -0500

    script edit to disable display.

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+echo "raspberry" | sudo -S xset s off
+echo "raspberry" | sudo -S xset -dpms
+echo "raspberry" | sudo -S xset s noblank
+
 cd /home/pi/yaaasss_bitch
 export DISPLAY=:0
 echo Getting Updates From Repository
